d8b07edf760b72e8ec9da363d6b66538663a48b0
zhang.junquan <zhang.junquan@chinaebi.com>
2019-04-25 17:41:22 +0800

    集群配置

--- bin/start-product-service.sh
+++ bin/start-product-service.sh
@@ -8,7 +8,7 @@ cd ..
 export BASE_PATH=$(cd "$(dirname "$0")"; pwd)
 cd $APP_PATH
 
-export cmd="java -Xms2048m -Xmx2048m -XX:NewRatio=2 -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+HeapDumpOnOutOfMemoryError  -Dspring.profiles.active=dev  -jar  $BASE_PATH/lib/poc-product-service-0.0.1-SNAPSHOT.jar"
+export cmd="java -Xms256m -Xmx256m -XX:NewRatio=2 -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+HeapDumpOnOutOfMemoryError  -Dspring.profiles.active=dev  -jar  $BASE_PATH/lib/poc-product-service-0.0.1-SNAPSHOT.jar"
 
 
 echo "execute [$cmd]"
