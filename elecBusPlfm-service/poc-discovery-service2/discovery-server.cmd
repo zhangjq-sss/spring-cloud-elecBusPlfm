b9a882b2ec3c9c0e0387e1148c4f7b67e8359f34
zhang.junquan <zhang.junquan@chinaebi.com>
2019-04-28 09:46:11 +0800

    删除eureka注册中心2 3

--- elecBusPlfm-service/poc-discovery-service2/discovery-server.cmd
+++
@@ -1,4 +0,0 @@
-cd /d %~dp0%
-start java   -Dspring.profiles.active=node1 -Ddc1=127.0.0.1 -Ddc2=127.0.0.1 -Ddc3=127.0.0.1  -jar   target/poc-discovery-service-0.0.1-SNAPSHOT.jar
-start java   -Dspring.profiles.active=node2 -Ddc1=127.0.0.1 -Ddc2=127.0.0.1 -Ddc3=127.0.0.1  -jar   target/poc-discovery-service-0.0.1-SNAPSHOT.jar
-start java   -Dspring.profiles.active=node3 -Ddc1=127.0.0.1 -Ddc2=127.0.0.1 -Ddc3=127.0.0.1  -jar   target/poc-discovery-service-0.0.1-SNAPSHOT.jar
