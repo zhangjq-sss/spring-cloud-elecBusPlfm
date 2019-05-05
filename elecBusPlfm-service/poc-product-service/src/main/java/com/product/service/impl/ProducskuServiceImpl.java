b806e6e9e5caf8e6f96120bf0b6b617a61f501c0
zhang.junquan <zhang.junquan@chinaebi.com>
2019-05-05 16:03:24 +0800

    增加乐观锁重试机制

--- elecBusPlfm-service/poc-product-service/src/main/java/com/product/service/impl/ProducskuServiceImpl.java
+++ elecBusPlfm-service/poc-product-service/src/main/java/com/product/service/impl/ProducskuServiceImpl.java
@@ -13,6 +13,7 @@ import com.domain.product.Producsku;
 import com.product.mapper.ProducskuMapper;
 import com.product.service.ProducskuService;
 
+import lombok.extern.slf4j.Slf4j;
 import tk.mybatis.mapper.entity.Example;
 
 /**
@@ -22,8 +23,12 @@ import tk.mybatis.mapper.entity.Example;
  * @email 18271632480@163.com
  * @date 2019-04-15 16:23:23
  */
+@Slf4j
 @Service
 public class ProducskuServiceImpl extends BaseBiz<ProducskuMapper,Producsku> implements ProducskuService{
+	
+	 //乐观锁冲突最大重试次数
+    private static final int DEFAULT_MAX_RETRIES = 1;
 
 	@Override
 	public Boolean validate(Producsku entity) {
@@ -46,30 +51,35 @@ public class ProducskuServiceImpl extends BaseBiz<ProducskuMapper,Producsku> imp
 	}
 
 	@Override
-	@Transactional
+//	@Transactional
 //	@LcnTransaction
 	public RrcResponse updateProdCount(Integer skuId, int count) {
-		//校验当前产品数量是否大于加入购物车数量
-		Producsku sku = selectById(skuId);
-		if (sku==null) {
-			return new RrcResponse(CodeMsg.POC_ERROR_PARAMETER);
-		}
-		int stock = sku.getStock().intValue();
-		if (stock==0||stock<count) {
-			return new RrcResponse(CodeMsg.POC_ERROR_STOCK_LOW);
-		}
-		Example example = new Example(Producsku.class);
-		Example.Criteria criteria = example.createCriteria();
-		criteria.andEqualTo("id", skuId);
-		criteria.andEqualTo("version", sku.getVersion());
-		Producsku record = new Producsku();
-		record.setStock(sku.getStock()-count);
-		record.setUpdTime(new Date());
-		record.setVersion(sku.getVersion()+1);
-		if (!(mapper.updateByExampleSelective(record, example)>0?true:false)) {
-			return new RrcResponse(CodeMsg.POC_ERROR_UPDATESTOCK);
-		}
+		int retriesTimes = 0;
+		do {
+			//校验当前产品数量是否大于加入购物车数量
+			Producsku sku = selectById(skuId);
+			if (sku==null) {
+				return new RrcResponse(CodeMsg.POC_ERROR_PARAMETER);
+			}
+			int stock = sku.getStock().intValue();
+			if (stock==0||stock<count) {
+				return new RrcResponse(CodeMsg.POC_ERROR_STOCK_LOW);
+			}
+			Example example = new Example(Producsku.class);
+			Example.Criteria criteria = example.createCriteria();
+			criteria.andEqualTo("id", skuId);
+			criteria.andEqualTo("version", sku.getVersion());
+			Producsku record = new Producsku();
+			record.setStock(sku.getStock()-count);
+			record.setUpdTime(new Date());
+			record.setVersion(sku.getVersion()+1);
+			retriesTimes++;
+			int ret = mapper.updateByExampleSelective(record, example);
+			if (ret>0) {
+				return new RrcResponse(CodeMsg.SUCCESS);
+			}
+		} while (retriesTimes<DEFAULT_MAX_RETRIES);
 //		int a = 1/0;
-		return new RrcResponse(CodeMsg.SUCCESS);
+		return new RrcResponse(CodeMsg.POC_ERROR_UPDATESTOCK);
 	}
 }
