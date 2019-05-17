a3c770473b8a6852d9ea8fb35f29407b04fa2e52
zhang.junquan <zhang.junquan@chinaebi.com>
2019-05-17 13:45:30 +0800

    sleuth/zipkin服务端优化 但不建议使用 尤其是springboot 2.0以后

--- elecBusPlfm-service/poc-sleuth-service/src/main/java/poc/sleuth/SleuthApplication.java
+++ elecBusPlfm-service/poc-sleuth-service/src/main/java/poc/sleuth/SleuthApplication.java
@@ -2,12 +2,13 @@ package poc.sleuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
-import zipkin.server.EnableZipkinServer;
-
-
-//@EnableZipkinServer
 @SpringBootApplication
+@EnableDiscoveryClient
+//@EnableZipkinServer  //zipkin服务器 默认使用http进行通信
+@EnableZipkinStreamServer //采用stream方式启动zipkin server ,也支持http通信 包含了@EnableZipkinServer,同时创建了rabbit-mq消息队列监听器
 public class SleuthApplication {
 
 	public static void main(String[] args) {
